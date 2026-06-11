24739c8b3523928e7aeddfb840c4cf7e4f709f74
Mayuresh P <mayure12sh12@gmail.com>
2026-06-12 01:58:19 +0530

    Fix: use working replica set connection string as the code-level fallback

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const uri = process.env.MONGODB_URI || 'mongodb+srv://mayure12sh12_db_user:McqyEZqD4rkwYeXj@iedindia1.isglc46.mongodb.net/ied-ims?retryWrites=true&w=majority';
+    const uri = process.env.MONGODB_URI || 'mongodb://mayure12sh12_db_user:McqyEZqD4rkwYeXj@ac-vhu35jb-shard-00-00.isglc46.mongodb.net:27017,ac-vhu35jb-shard-00-01.isglc46.mongodb.net:27017,ac-vhu35jb-shard-00-02.isglc46.mongodb.net:27017/ied-ims?ssl=true&replicaSet=atlas-113or1-shard-0&authSource=admin&retryWrites=true&w=majority';
     console.log('[INFO] Connecting to MongoDB...');
     const conn = await mongoose.connect(uri, {
       serverSelectionTimeoutMS: 5000,
